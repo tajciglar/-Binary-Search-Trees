e2860947bd3a362072e81bf7a32a32c921a56a9e
tajciglar <78543798+tajciglar@users.noreply.github.com>
2023-12-24 06:38:18 +0000

    Add delete function

--- script.js
+++ script.js
@@ -51,29 +51,74 @@ class Tree {
         console.log(this.root);
 
     }
+
+    delete(value, element = this.root) {
+        console.log(this.root);
+        if (element === null){
+            return element;
+        }
+        // Traverse the tree
+        console.log(this.root);
+        if(element.data < value){
+            element.rightChild = this.delete(value, element.rightChild)
+            
+        } else if (element.data > value){
+            element.leftChild = this.delete(value, element.leftChild)
+        } else {
+            if(element.leftChild === null){
+
+                return element.rightChild;
+
+            } else if (element.rightChild === null) {
+
+                return element.leftChild;
+
+            } else {
+                
+                const min = findMin(element)
+                element.data = min.data;
+                element.rightChild = this.delete(min.data, element.rightChild);
+               
+            }
+        } 
+        return element;
+    }
 }
 
-
-const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 161, 20, 22];
-const newArray = [...new Set(array)]
-newArray.sort((a, b) => a - b)
-
-const tree = new Tree();
-tree.buildTree(newArray, 0 , newArray.length-1);
-
+// Function that shows the tree in console
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
+    if (!node) {
         return;
     }
-    if (node.rightChild !== null) {
+    if (node.rightChild !== undefined) {
         prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.leftChild !== null) {
+    if (node.leftChild !== undefined) {
         prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
 };
 
+
+// Array, sort array and remove duplicates
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 161, 20, 22];
+const newArray = [...new Set(array)]
+newArray.sort((a, b) => a - b)
+
+// Create a tree
+const tree = new Tree();
+tree.buildTree(newArray, 0 , newArray.length-1);
+
+
+
+// Function to find the minimum 
+function findMin(element){
+    element = element.rightChild;
+    while(element.leftChild !== null) {
+        element = element.leftChild;
+    }
+    return element;
+}
 
 prettyPrint(tree.root);
 tree.insert(40);
@@ -81,5 +126,11 @@ prettyPrint(tree.root);
 tree.insert(27)
 prettyPrint(tree.root);
 tree.insert(21)
+
 prettyPrint(tree.root);
+
+prettyPrint(tree.root);
+tree.delete(22)
+prettyPrint(tree.root);
+
 
